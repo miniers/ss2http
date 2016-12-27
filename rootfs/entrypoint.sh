015466f6de944426ae52d7c00bda16d21eb95254
liukai <659801@qq.com>
2016-12-27 13:15:42 +0800

    修复sslocal启动失败

--- rootfs/entrypoint.sh
+++ rootfs/entrypoint.sh
@@ -4,7 +4,7 @@
 # Configure the service:
 #------------------------------------------------------------------------------
 env /usr/local/bin/ss-local -s $SERVER_ADDR -p $SERVER_PORT -k $PASSWORD \
-  -b 0.0.0.0 -l ${LOCAL_PORT:-8124} -m ${METHOD:-'chacha20'} \
-  -d start
+  -b 0.0.0.0 -l ${LOCAL_PORT:8124} -m ${METHOD:'chacha20'} \
+  -A  > /dev/null 2>&1 &
 
 env /usr/sbin/privoxy --no-daemon /etc/privoxy/config
