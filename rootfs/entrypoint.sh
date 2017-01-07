8aca336413dd4880d59f22297408910a269f4b4e
liukai <659801@qq.com>
2017-01-08 01:05:00 +0800

    优化配置文件位置

--- rootfs/entrypoint.sh
+++ rootfs/entrypoint.sh
@@ -3,10 +3,10 @@
 #------------------------------------------------------------------------------
 # Configure the service:
 #------------------------------------------------------------------------------
-env /usr/sbin/haproxy -f /etc/haproxy/config.cfg
+env /usr/sbin/haproxy -f /etc/ss/haproxy/config.cfg
 
 env /usr/local/bin/ss-local -s $SERVER_ADDR -p $SERVER_PORT -k $PASSWORD \
   -b 0.0.0.0 -l ${LOCAL_PORT:-8124} -m ${METHOD:-'chacha20'} \
   -A  > /dev/null 2>&1 &
 
-env /usr/sbin/privoxy --no-daemon /etc/privoxy/config
+env /usr/sbin/privoxy --no-daemon /etc/ss/privoxy/config
